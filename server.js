90205cde961ed3572a2b8638cb8448cd7c73336e
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2020-02-18 23:03:07 -0300

    Novas configurações JS
    Novas configurações JS

--- server.js
+++ server.js
@@ -1,8 +1,40 @@
+//configurando o servidor
 const express = require("express")
 const server = express()
 
+//configurar o servidor para apresentar arquivos estaticos (css, imagens...)
+server.use(express.static('public'))
+
+//configurando a template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+}) //indica que o index html está na raiz do projeto
+
+const doadores= [
+    {
+        nome: "Larissa",
+        sangue: "A+"
+    },
+    {
+        nome: "Christian",
+        sangue: "B-"
+    },
+    {
+        nome: "Guilherme",
+        sangue: "AB-"
+    },
+    {
+        nome: "Leonardo",
+        sangue: "O+"
+    }
+]
+
+//configurar a apresentação da página
 server.get("/", function(request, response){
-    return response.send("ok");
+    return response.render("index.html", {doadores});
 })
 
+//ligar o sservidor e permitir acesso na porta 3000
 server.listen(3000)
