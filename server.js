bd231381d722e2d117c80793160af556e36fcaee
Larissa Martins Mauricio <larissa.jucre@gmail.com>
2020-02-18 23:28:46 -0300

    .
    .

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = express()
 //configurar o servidor para apresentar arquivos estaticos (css, imagens...)
 server.use(express.static('public'))
 
+//habilitar corpo do form
+server.use(express.urlencoded({extended: true}))
+
 //configurando a template engine
 const nunjucks = require("nunjucks")
 nunjucks.configure("./", {
@@ -36,5 +39,17 @@ server.get("/", function(request, response){
     return response.render("index.html", {doadores});
 })
 
+server.post("/", function(request, response){
+    const nome = request.body.nome
+    const email = request.body.email
+    const tipoSanguineo = request.body.tipoSanguineo
+
+    doadores.push({
+        nome: nome,
+        sangue: tipoSanguineo,
+    })
+
+    return response.redirect("/")
+})
 //ligar o sservidor e permitir acesso na porta 3000
 server.listen(3000)
